d9cfeb34acc7d5384eec2af62db47fe005538b53
Rebun <rubengdopazo@gmail.com>
2023-09-18 11:08:21 +0100

    Added countdown to start the game A 3 seconds countdown widget appears at the start of the game. Ruben Gomez Dopazo

--- Source/ToonTanks/ToonTanksGameMode.h
+++ Source/ToonTanks/ToonTanksGameMode.h
@@ -20,6 +20,9 @@ public:
 protected:
 	virtual void BeginPlay() override;
 
+	UFUNCTION(BlueprintImplementableEvent)
+	void StartGame();
+
 private:
 	class ATank* Tank;
 	class AToonTanksPlayerController* ToonTanksPlayerController;
